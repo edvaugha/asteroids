03661cf11c0dd7a0727b91fdfe4235927a35de4f
edvaugha <tuna517@gmail.com>
2025-01-11 13:14:41 -0500

    Added sprites for player and defined movement

--- main.py
+++ main.py
@@ -3,20 +3,42 @@
 # throughout this file
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    print("Starting game...")  # Add this
     pygame.init()
+    pygame.display.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    print(f"Screen dimensions: {SCREEN_WIDTH} x {SCREEN_HEIGHT}")  # Add this
+    dt = 0
+    clock = pygame.time.Clock()
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updateable, drawable)
+    player_one = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    print("Player created")  # Add this
+
+
+
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+            
+        screen.fill("black")
+        for thing in updateable:
+            thing.update(dt)
+        #player_one.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+        #player_one.draw(screen)
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
